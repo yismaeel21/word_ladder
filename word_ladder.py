dfb4dacb43284826d23abd3cf4dc1ac048c47da8
yismaeel21 <52178055+yismaeel21@users.noreply.github.com>
2020-02-19 20:17:08 -0800

    Update word_ladder.py

--- word_ladder.py
+++ word_ladder.py
@@ -34,7 +34,7 @@ def verify_word_ladder(ladder):
     Returns True if each entry of the input list is adjacent to its neighbors;
     otherwise returns False.
     '''
-    if len(ladder) <= 1:
+    if len(ladder) == 0:
         return False
     for k in range(len(ladder)-1):
         if not _adjacent(ladder[k], ladder[k+1]):
