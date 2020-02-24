d3eff865d32559690b1017988bd110c2f0e827d1
yismaeel21 <52178055+yismaeel21@users.noreply.github.com>
2020-02-24 11:33:06 -0800

    Update word_ladder.py

--- word_ladder.py
+++ word_ladder.py
@@ -24,7 +24,7 @@ def word_ladder(start_word, end_word, dictionary_file='words5.dict'):
    
     while len(ourQ) != 0:        #while our queue is non-empty
         top = ourQ.pop()
-        for word in words_copy: #for each word in the dictionary
+        for word in words: #for each word in the dictionary
             if _adjacent(word,top[-1]):
                 stackCopy = copy.deepcopy(top)
                 stackCopy.append(word)
@@ -35,7 +35,7 @@ def word_ladder(start_word, end_word, dictionary_file='words5.dict'):
                             stackCopy.pop(i)
                     return (stackCopy)
                 ourQ.appendleft(stackCopy)
-        words.remove(word)
+            words.remove(word)
 
 
 def verify_word_ladder(ladder):
